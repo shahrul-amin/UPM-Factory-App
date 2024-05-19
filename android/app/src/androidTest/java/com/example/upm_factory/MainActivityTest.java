7a6a8da731c982bec2d352c4dd3958a56ec9bba6
shahrul-amin <shahrulamin8776@gmail.com.com>
2024-05-19 12:25:42 +0800

    adding dependencies and minor changes

--- android/app/src/androidTest/java/com/example/upm_factory/MainActivityTest.java
+++ android/app/src/androidTest/java/com/example/upm_factory/MainActivityTest.java
@@ -1,9 +1,15 @@
-package com.example.factory_app;
+package com.example.upm_factory;
 
 import androidx.test.rule.ActivityTestRule;
 import dev.flutter.plugins.integration_test.FlutterTestRunner;
 import org.junit.Rule;
 import org.junit.runner.RunWith;
+
+@RunWith(FlutterTestRunner.class)
+public class MainActivityTest {
+  @Rule
+  public ActivityTestRule<MainActivity> rule = new ActivityTestRule<>(MainActivity.class, true, false);
+}
 
 @RunWith(FlutterTestRunner.class)
 public class MainActivityTest {
